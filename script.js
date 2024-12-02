4f1158dec71e1092173acacf5b23d68960ef9d45
Vishal Gupta <guptavishal9137@gmail.com>
2024-12-02 22:01:37 +0530

    Testimonials Slider

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-(()=>{
-    const customerImage = document.getElementById("customer-mage")
-    const customerName = document.getElementById("customer-name")
-    const customerText =document.getElementById("customer-text")
-    const btn = document.querySelectorAll(".btn")
+(function (){
+    const customerImage = document.querySelector("#customer-image");
+    const customerName = document.querySelector("#customer-name");
+    const customerText =document.querySelector("#customer-text");
+    const btn = document.querySelectorAll(".btn");
+    
+    let  index = 0
+    const customers = [];
 
     function Customer(img, name, text){
         this.img =img
@@ -11,11 +14,12 @@
     }
 
 
+
     function createCustomer(img,name,text){
         let Img  =`./Gallery/${img}.png`
-        let customer = new Customer(img, name, text)
+        let customer = new Customer(Img, name, text)
 
-        customer.push(customer)
+        customers.push(customer)
     }
 
     
@@ -23,9 +27,33 @@
     createCustomer(1,'Sam','Lorem Esse consequatur, iusto quibusdam, eligendi ex ullam fugiat necessitatibus tempora nemo animi quaerat quo quidem minus totam facilis reiciendis. Tempore, a veritatis!')
     createCustomer(2,'David','Lorem ipsum dolor sit amet consectetur adipisicing elit. ex ullam fugiat necessitatibus tempora nemo animi quaerat quo quidem minus totam facilis reiciendis. Tempore, a veritatis!')
     createCustomer(3,'Kane','Lorem ipsum dolor sit  Esse consequatur, iusto quibusdam, eligendi ex ullam fugiat necessitatibus tempora nemo animi quaerat quo quidem minus totam facilis reiciendis. Tempore, a veritatis!')
-    createCustomer(4,'Jake','Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse consequatur, iusto quibusdam, eligendi ex ullam fugiat necessitatibus tempora nemo. Tempore, a veritatis!')
+    createCustomer(5,'Jake','Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse consequatur, iusto quibusdam, eligendi ex ullam fugiat necessitatibus tempora nemo. Tempore, a veritatis!')
    
+    btn.forEach((button)=>{
+        button.addEventListener("click", (e)=>{
+            if (e.target.parentElement.classList.contains('preBtn')){
+                if(index === 0){
+                    index=customers.length
+                }
+                index--
+                customerImage.src = customers[index].img
+                customerName.textContent = customers[index].name
+                customerText.textContent = customers[index].text
+            }
 
+            if(e.target.parentElement.classList.contains('nextBtn')){
+                index++
+                if(index === customers.length){
+                    index = 0
+                }
+
+                customerImage.src = customers[index].img
+                customerName.textContent = customers[index].name
+                customerText.textContent = customers[index].text
+            }
+
+        })
+    })
 
 
 })()
